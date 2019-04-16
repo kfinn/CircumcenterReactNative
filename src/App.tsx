fcf1ae430aa2af313588276e4ad997524a7072e2
Kevin Finn <superkevin@gmail.com>
2019-04-15 20:42:53 -0400

    tslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import * as React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
+  public render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
     justifyContent: 'center',
   },
-})
+});
