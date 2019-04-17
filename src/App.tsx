fccba1f23d05803b0c3f724955d75fa68a91bb4c
Kevin Finn <superkevin@gmail.com>
2019-04-17 00:39:56 -0400

    clicking a button creates an event

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
 
 export default class App extends React.Component {
+  onPress = () => {
+    fetch(
+      "http://localhost:3000/api/events",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          event: {
+            start: "2019-05-01 12:00:00"
+          }
+        })
+      }
+    )
+    .then(response => response.json())
+    .then(console.log)
+  }
+
   public render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Button title="New Event" onPress={this.onPress} />
       </View>
     );
   }
